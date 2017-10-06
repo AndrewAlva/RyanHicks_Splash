c085fd196b4b320031cd59cbe04fa66566634bd3
Mandelbrot Studio <mandelbrot@iMac-de-Mandelbrot.local>
2017-10-05 22:02:21 -0500

    Responsive done. Working on motion intro

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// var counter = 0;
+
+// setInterval(function(){
+// 	console.log(counter + " segundos");
+// 	counter += 1;
+// }, 1000);
+
+// $("#mbrtWrap").load(function(){
+// 	console.log("Images loaded");
+// });
